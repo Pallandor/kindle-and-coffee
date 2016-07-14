87912b5f701d456bbd253a93b99d60f7da99dc44
Pallandor <roger.sejas@gmail.com>
2016-07-14 17:42:23 +1000

    Clean server code and separate into folders

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import path from 'path';
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.get('*', (req,res) => res.send('Testing server')); 
+app.get('*', (req,res) => res.send('Testing server 2.0')); 
 
 app.listen(app.get('port'), () => {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
