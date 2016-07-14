87741cb0adcedda59b168ee535733c61d85ae2e3
Pallandor <roger.sejas@gmail.com>
2016-07-14 17:31:28 +1000

    Fix server to serve test response

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.get('*', (req,res) => res.send('Testing server')); 
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
