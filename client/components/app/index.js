b4136893868e08d47dbb8381a9a63acd8abfe70f
Pallandor <roger.sejas@gmail.com>
2016-07-15 04:45:20 +1000

    Add working inline iframe to App component

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
-const testClick = () => alert('hello!'); 
+const url = 'https://www.coffeecompany.com.au/coffee/organic-coffee';
+const iframeStyle = {
+  border: 0,
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+};
 
 const App = () => (
   <div>
     <p>Hello Coffee Automation! From App!</p>
-    <button onClick={testClick}>Click me to order your 1kg of coffee!</button>
+    <button onClick={()=>alert('does nothing for now!')}>Click me to order your 1kg of coffee!</button>
+    <iframe src={url} style={iframeStyle}>Sorry, your browser does not support our technology!</iframe>
   </div>
 );
 
-export default App; 
+export default App;
