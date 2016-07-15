96f2c51d407938b40424a2858aa48fd8acd87f2e
Pallandor <roger.sejas@gmail.com>
2016-07-15 10:35:40 +1000

    Automate navigation up to Checkout page
    Former-commit-id: 758e0633f42ab0525dbe79470c8b470d71538a60

--- server/automation/index.js
+++ server/automation/index.js
@@ -1,16 +1,23 @@
 import browser, * as config from './webdriver.config.js';
 
+// NOTE: waitForExist appears to continue running after click chain call. Hm.
+
 const automationHandler = (req, res) => {
   browser
     .init()
     .url(config.url)
     .selectByAttribute('#grindOption', 'value', '79')
-    // .setValue(config.searchBar, config.message)
-    // .click(config.buttonByName)
-    // .getTitle()
-    // .then(title => res.send(`The title received was ${title}`))
+    .selectByAttribute('#sizeOption', 'value', '5')
+    .click('#AddToBag')
+    .waitForExist('#bag-CTA[class="btn-holder checkout"]', 10000)
+    .click('#Checkout')
+    // .waitForExist('a.btn[href="/checkout"]')   // may not be required, try without.
+    .click('a.btn[href="/checkout"]')
     .pause(config.waitTime)
     .end();
 };
+
+// wait for btn-holder checkout
+
 
 export default automationHandler;
--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ import automationHandler from './automation';
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.get('/getcoffee', automationHandler); 
+
+app.get('/getcoffee', automationHandler);
 
 app.listen(app.get('port'), () => {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
