25426ebf7d2a27d5c288dc6fc899b6a4ff2a2063
Pallandor <roger.sejas@gmail.com>
2016-07-16 06:56:56 +1000

    Add server routes to serve static files to client
    Former-commit-id: 88f93b0faec8d73a78064720e2eca9546a9248fe

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import automationHandler from './automation';
+import path from 'path';
 
 const app = express();
 
+const distPath = path.join(__dirname, '..', 'dist');
+const indexFileName = 'index.html';
+
 app.set('port', process.env.PORT || 3000);
+
 app.get('/getcoffee', automationHandler);
+
+app.use(express.static(distPath));
+app.get('*', (req, res) => res.sendFile(path.join(distPath, indexFileName)));
 
 app.listen(app.get('port'), () => {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
