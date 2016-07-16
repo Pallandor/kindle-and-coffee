5e4eefbf8e8a96cb939b9b1d62e03d17eedc36f3
Pallandor <roger.sejas@gmail.com>
2016-07-16 12:12:50 +1000

    Setup before, after async hooks to Selenium startup and URL test in Mocha
    * Modify test suite timeout to suitably deal with selenium startup/browser automated navigation delays.
    * Start Selenium server in Before hook, track child_process vis closure, kill to shutdown Selenium instance.
    * Ensure URL test correctly fails on non-match.
    * REMOVE ES6 arrow functions (Note: Mocha discourages use, interferes with passing of context across functions).
    
    
    Former-commit-id: ac5783a56eb88f0f8772dbe374cafe9da7765f7a

--- server/automation/selenium.js
+++ server/automation/selenium.js
@@ -3,9 +3,12 @@ import selenium from 'selenium-standalone';
 const installConfig = {};
 
 const startSelenium = callback => {
+  console.log("inside start selenium!!");
   selenium.install(installConfig, (installError) => {
+    console.log('inside selenium install!');
     if (installError) return callback(installError);
     selenium.start((startErr, childInstance) => {
+      console.log('++ inside ACTUAL selenium.start!');
       if (startErr) return callback;
       childInstance.stderr.on('data', function(data) {
         console.log(data.toString());
--- server/test/automations.test.js
+++ server/test/automations.test.js
@@ -1,22 +1,40 @@
 import { expect } from 'chai';
 
-describe('## Automation Tasks', function() {
-  describe('-- Before Hook --', function() {
-    // before(function(done) {
-    //   // beforehook
-    // });
-    describe('# Inner Test', function() {
-      it('should start empty', function(done) {
-        expect(true).to.equal(true);
-        done();
-      });
+import browser from '../automation/webdriver.config';
+import * as automate from '../automation/automations';
+import startSelenium from '../automation/selenium';
 
-      it('should run tests in succession', function(done) {
-        expect(true).to.equal(true);
-        done();
-      });
+describe('## Automation Tasks for coffeecompany.com.au', function() {
+  let childSeleniumProcess = null;
+  this.timeout(25000);
+  before(function(done) {
+    startSelenium(function(err, child) {
+      if (err) return done(err);
+      childSeleniumProcess = child;
+      done();
     });
-
   });
 
+  describe('# getHomepage --', function() {
+    it('should navigate to the correct url', function(done) {
+      browser
+        .then(automate.getHomepage(browser))
+        .getUrl()
+        .then(function(retrievedUrl) {
+          console.log(`inside the part where retrievedUrl is: ${retrievedUrl}`);
+          expect(retrievedUrl).to.equal('https://www.coffeecompany.com.au/coffee/flavoured/swiss-chocolate');
+          done();
+        });
+    });
+
+    it('should run tests in succession', function(done) {
+      expect(true).to.equal(true);
+      done();
+    });
+  });
+
+  after(function(done){
+    childSeleniumProcess.kill();
+    done();
+  });
 });
